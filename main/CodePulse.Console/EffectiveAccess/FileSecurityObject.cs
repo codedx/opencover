97a5a32fbd5e40cb7d9c4a06edac64368312447c
Steve Salas <ssalas@codedx.com>
2018-02-02 17:20:00 -0500

    Inserted license header in added files

--- main/CodePulse.Console/EffectiveAccess/FileSecurityObject.cs
+++ main/CodePulse.Console/EffectiveAccess/FileSecurityObject.cs
@@ -1,4 +1,11 @@
-﻿using System.Security.AccessControl;
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved
+
+using System.Security.AccessControl;
 
 namespace CodePulse.Console.EffectiveAccess
 {
